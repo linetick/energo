1d15c86e1903b358eedd33c42124bcdde9d77f79
Pioneer228 <daad300604@gmail.com>
2025-06-27 18:01:52 +0700

    Снова коммит

--- bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
+++ bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001750845376';
-$dateexpire = '001750848976';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1750846632;}';
+$datecreate = '001750847567';
+$dateexpire = '001750851167';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1750848964;}';
 return true;
 ?>
--- bitrix/managed_cache/MYSQL/orm_b_sec_wwall_rules/82/829e168f268d54955d65b868c1387f41.php
+++ bitrix/managed_cache/MYSQL/orm_b_sec_wwall_rules/82/829e168f268d54955d65b868c1387f41.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001750846173';
-$dateexpire = '001750846178';
+$datecreate = '001750847561';
+$dateexpire = '001750847566';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:1:{i:0;a:1:{s:3:"CNT";s:1:"0";}}}';
 return true;
 ?>
