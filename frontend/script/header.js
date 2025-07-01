fc0d4e2b79f911c78ab0884938c3f273d86077c8
Pudge <linetick@mail.ru>
2025-07-01 16:28:15 +0700

    Header сделан на 90 процентов

--- frontend/script/header.js
+++ frontend/script/header.js
@@ -1,10 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
   const menuToggle = document.querySelector(".menu-toggle");
   const headerTop = document.querySelector(".header-top");
+  const logoPhone = document.querySelector(".logo-phone"); // Получаем элемент логотипа
 
   menuToggle.addEventListener("click", function () {
+    // Переключаем классы active для меню и кнопки
     this.classList.toggle("active");
     headerTop.classList.toggle("active");
+
+    // Переключаем видимость логотипа
+    logoPhone.classList.toggle(
+      "hidden",
+      headerTop.classList.contains("active")
+    );
 
     // Блокировка прокрутки страницы при открытом меню
     if (headerTop.classList.contains("active")) {
@@ -21,6 +29,7 @@ document.addEventListener("DOMContentLoaded", function () {
       if (window.innerWidth <= 773) {
         menuToggle.classList.remove("active");
         headerTop.classList.remove("active");
+        logoPhone.classList.remove("hidden"); // Показываем логотип при закрытии
         document.body.style.overflow = "";
       }
     });
